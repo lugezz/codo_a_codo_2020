64158675b924cc994e68710e9047e322ee691160
Eugenio <41469771+lugezz@users.noreply.github.com>
2022-06-08 06:18:48 -0300

    First Commit

---
+++ MalditoVue/aaaa/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Esta es la página del About</h1>
+  </div>
+</template>
---
+++ MalditoVue/scripts.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: "Aprendiendo Vue!",
+      prueba2: "Probando",
+      prueba3: "Si funciona soy un genio",
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue.js' },
+        { text: 'Build Something Awesome' },
+        { text: 'Hacer algo que sirva y dejar de jugar' }
+      ],
+      ok: Date.now()>"2021-01-01",
+      fecha: Date.now(),
+      counter: 0
+    }
+})
